9a3e1c6e38fd6afe9a11809de84c84e57e9b6f19
Alex Janka <alex@alexjanka.com>
2017-06-02 19:07:34 +1000

    fix formatting

--- index.ts
+++ index.ts
@@ -3,11 +3,11 @@ import expresshb = require('express-handlebars');
 
 var app = express();
 
-app.engine('handlebars', expresshb({defaultLayout: 'main'}));
+app.engine('handlebars', expresshb({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
- 
+
 app.get('/', function (req, res) {
-    res.render('home');
+	res.render('home');
 });
 
 app.listen(3000);
