5876ba347fd0cb496b1b713b7bf62fec18d8b8eb
Alex Janka <alex@alexjanka.com>
2017-06-02 19:00:22 +1000

    First working server

---
+++ index.ts
@@ -0,0 +1,13 @@
+import express = require('express');
+import expresshb = require('express-handlebars');
+
+var app = express();
+
+app.engine('handlebars', expresshb({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+ 
+app.get('/', function (req, res) {
+    res.render('home');
+});
+
+app.listen(3000);
