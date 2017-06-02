8e4ecd3773396f7bb21a9a54804ae54e9b43a31c
Alex Janka <alex@alexjanka.com>
2017-06-02 21:16:55 +1000

    Getting info works as a promise!!!!!!

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@ import express = require('express');
 import expresshb = require('express-handlebars');
 import http = require('http');
 import https = require('https');
-import promise = require('promise');
+import Promise = require('promise');
 
 import { IPGeoJson } from './ip_geo';
 
@@ -12,48 +12,38 @@ app.engine('handlebars', expresshb({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
 app.get('/', function (req, res) {
-	res.render('home');
-	getIpInfo("::ffff:" + "119.17.156.106").country; //replace with req.ip
+	getIpInfo("::ffff:" + "119.17.156.106").done(function (geoinfo: IPGeoJson) {
+		res.render('home');
+	}); //replace with req.ip
 });
 
 app.listen(3000);
 
-function getIpInfo(ip: String): IPGeoJson {
-	var ipinfo: IPGeoJson = {
-		ip: "",
-		city: "",
-		region: "",
-		country: "",
-		loc: "",
-		postal: ""
-	};
-
-	var options = {
-		host: 'ipinfo.io',
-		port: 443,
-		path: '/' + ip + '/geo',
-		method: 'GET',
-		headers: {
-			'Content-Type': 'application/json'
+function getIpInfo(ip: String): Promise.IThenable<{}> {
+	return new Promise(function (resolve, reject) {
+		var options = {
+			host: 'ipinfo.io',
+			port: 443,
+			path: '/' + ip + '/geo',
+			method: 'GET',
+			headers: {
+				'Content-Type': 'application/json'
+			}
 		}
-	}
-	console.log(options.path);
 
-	var callback = function (response: http.IncomingMessage) {
-		var str: string;
+		var callback = function (response: http.IncomingMessage) {
+			var str: string;
 
-		response.on('data', function (chunk) {
-			str += chunk;
-		});
+			response.on('data', function (chunk) {
+				str += chunk;
+			});
 
-		response.on('end', function () {
-			console.log(str);
-			ipinfo = <IPGeoJson>JSON.parse(str.slice(9));
-			console.log(ipinfo.city);
-		});
-	}
+			response.on('end', function () {
+				console.log(str);
+				resolve(<IPGeoJson>JSON.parse(str.slice(9)));
+			});
+		}
 
-	https.get(options, callback);
-
-	return ipinfo;
+		https.get(options, callback);
+	})
 }
